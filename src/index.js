b0ecfb63205f84cc4cbde05772f1c0eb0770f17b
delaneytiger21 <forsythedelaney@gmail.com>
2022-04-12 15:25:59 -0500

    Added API Integration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// create variable to find the element and create a variable to get the current date/time then replace HTML with current date and send current time to Format Date function to reconfigure date variables
-// create the function, it is receiving a date. create variable to get the hours/minutes to convert time and use day array to convert date then return variables
 function formatDate(date) {
   let hours = date.getHours();
   if (hours < 10) {
@@ -22,35 +20,47 @@ function formatDate(date) {
   let day = days[dayIndex];
   return `${day} ${hours}:${minutes}`;
 }
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
 
-// create variable to find the form and add an Event Listener "submit" and function "search"
-// create the function search and it will receive an event, prevent the default behavior, create variable to search for the elements and repalce HTML with the values
+function showWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
 function search(event) {
   event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
+  let apiKey = "80b5b4bfb3171079c6d323d0bf6f3213";
+  let city = "Austin";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(url).then(showWeather);
 }
-let searchForm = document.querySelector("#search-bar");
-searchForm.addEventListener("submit", search);
 
-// create varibale to find element and add Event Listener to element with "click" and function name
-// create function that receives and event, prevent the default, create a variable to find the element to replace the HTML with the temp
 function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 90;
 }
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
 function convertToCelsius(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = 32;
 }
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchBar = document.querySelector("#search-bar");
+searchBar.addEventListener("submit", search);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
