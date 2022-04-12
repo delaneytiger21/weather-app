00fc04477c6f88f3f0476e62a4d13b4bbf713d37
delaneytiger21 <89812496+delaneytiger21@users.noreply.github.com>
2022-04-12 18:17:41 -0500

    Added Current Location Button

--- src/index.js
+++ src/index.js
@@ -21,25 +21,33 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+
 function showWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#description").innerHTML = response.data.weather[0].main;
+}
+function search(city) {
+  let apiKey = "80b5b4bfb3171079c6d323d0bf6f3213";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city)
 }
 
-function search(event) {
+function searchLocation(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
+}
+function getCurrentLocation(event) {
   event.preventDefault();
-  let apiKey = "80b5b4bfb3171079c6d323d0bf6f3213";
-  let city = "Austin";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(url).then(showWeather);
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 function convertToFahrenheit(event) {
@@ -53,14 +61,22 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 32;
 }
 
+
 let dateElement = document.querySelector("#date");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 
 let searchBar = document.querySelector("#search-bar");
-searchBar.addEventListener("submit", search);
+searchBar.addEventListener("submit", handleSubmit);
+search("Austin");
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+
+
