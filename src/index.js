e545c9f54d025c59ee1351687a45c9146a00040b
delaneytiger21 <89812496+delaneytiger21@users.noreply.github.com>
2022-04-16 19:26:42 -0500

    finalized html css & js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ function formatDay(timestamp) {
   let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   return days[day];
 }
-
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -25,8 +24,6 @@ function displayForecast(response) {
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
-
-  
 function getForecast(coordinates) {
   let apiKey = "343e26b3ffb2c17a61d6a1123defd48c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
